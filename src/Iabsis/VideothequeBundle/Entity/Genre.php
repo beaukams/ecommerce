35ce8fb858c87fa8cf8c426baee77090ecd040dd
prosperitos <p.kiemde@yahoo.fr>
2016-06-29 14:19:41 +0200

    Valider

---
+++ src/Iabsis/VideothequeBundle/Entity/Genre.php
@@ -0,0 +1,116 @@
+<?php
+# Fichier Genre.php
+
+namespace Iabsis\VideothequeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+    /**
+     * @ORM\Entity(repositoryClass="Iabsis\VideothequeBundle\Entity\GenreRepository")
+     */
+    
+class Genre
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * Bidirectional 
+     *
+     * @ORM\ManyToMany(targetEntity="Film", mappedBy="listeDesGenres")
+     */
+    protected $listeDesFilms;
+
+    /**
+     * @ORM\Column(type="string", length=90)
+     */
+    protected $label;
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->listeDesFilms = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set label
+     *
+     * @param string $label
+     * @return Genre
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * Get label
+     *
+     * @return string 
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * Add listeDesFilms
+     *
+     * @param \Iabsis\VideothequeBundle\Entity\Film $listeDesFilms
+     * @return Genre
+     */
+    public function addListeDesFilm(\Iabsis\VideothequeBundle\Entity\Film $listeDesFilms)
+    {
+        $this->listeDesFilms[] = $listeDesFilms;
+
+        return $this;
+    }
+
+    /**
+     * Remove listeDesFilms
+     *
+     * @param \Iabsis\VideothequeBundle\Entity\Film $listeDesFilms
+     */
+    public function removeListeDesFilm(\Iabsis\VideothequeBundle\Entity\Film $listeDesFilms)
+    {
+        $this->listeDesFilms->removeElement($listeDesFilms);
+    }
+
+    /**
+     * Get listeDesFilms
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getListeDesFilms()
+    {
+        return $this->listeDesFilms;
+    } 
+
+   /**
+ * Affichage d'une entité Genre avec echo
+ * @return string Représentation du genre
+ */
+ public function __toString()
+ {
+    return $this->label;
+}
+} 
+
+
---
+++ src/Iabsis/VideothequeBundle/Form/FilmType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Iabsis\VideothequeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class FilmType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('listeDesGenres')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Iabsis\VideothequeBundle\Entity\Film'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'iabsis_videothequebundle_film';
+    }
+}
