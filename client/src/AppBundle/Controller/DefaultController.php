bcffc0010f5bc10fe3153d08f4f958db5abc7a75
prosperitos <p.kiemde@yahoo.fr>
2016-08-13 17:52:39 +0200

    bonjour

--- client/src/AppBundle/Controller/DefaultController.php
+++ client/src/AppBundle/Controller/DefaultController.php
@@ -18,12 +18,4 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
     }
-
-    /**
-     * @Route("/contactus", name="contactus")
-     */
-    public function contactusAction(Request $request)
-    {
-        return $this->render('contactus.html.twig');
-    }
 }
